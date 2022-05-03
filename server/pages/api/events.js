a07603fffed34fffc28ef0cf93a6bdc570cf4325
bsord <brandon.sorgdrager@gmail.com>
2022-05-03 15:12:40 -0500

    Initial web interface code

--- server/pages/api/events.js
+++ server/pages/api/events.js
@@ -3,6 +3,12 @@ var aws = require('aws-sdk')
 var multer = require('multer')
 var multerS3 = require('multer-s3')
 var Busboy = require('busboy');
+var mongoose = require('mongoose');
+var Events = require('../../models/event_model.js');
+// Mongoose
+mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 aws.config.update({
   secretAccessKey: process.env.S3_SECRETACCESSKEY,
@@ -22,31 +28,49 @@ var upload = multer({
       cb(null, file.originalname);
     }
   })
-}).any('file', 1);
+}).fields([
+  {name:'image', maxCount: 1},
+  {name:'video', maxCount: 1}
+]);
 
-async function getFields(req) {
+async function getBodyData(req) {
   var fields = {}
+  var files = []
   return new Promise((resolve) => {
     const busboy = Busboy({ headers: req.headers });
+    busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+      files[fieldname] = {fieldname: fieldname, filename: filename, encoding: encoding, mimetype: mimetype}
+      file.on('data', function (data) {});
+      file.on('end', function () {});
+    });
     busboy.on('field', function (fieldname, val) {
       fields[fieldname] = val
     });
     busboy.on('finish', function () {
-      resolve(fields);
+      resolve([fields,files]);
     });
     req.pipe(busboy);
   });
 }
 
 export default async function handler(req, res) {
-  
 
   const { method } = req;
 
   switch (method) {
+
+
     case 'GET':
-      res.json({ method: 'GET', endpoint: 'Events' });
+      Events.find({}, function (err, events) {
+        if (err) {
+          res.json({msg: 'error'});
+        };
+        res.json(events);
+      })
+      
       break;
+
+
     case 'POST':
       await upload(req, res, function (error) {
         if (error) {
@@ -55,11 +79,17 @@ export default async function handler(req, res) {
         }
       });
 
-      const fields = await getFields(req)
-      console.log(fields)
+      const [fields,files] = await getBodyData(req)
+      
+      const imagePath = 'https://' + process.env.S3_BUCKET + '.' + process.env.S3_ENDPOINT + '/' + files['image'].filename
+      const videoPath = 'https://' + process.env.S3_BUCKET + '.' + process.env.S3_ENDPOINT + '/' + files['video'].filename
 
-      console.log('File uploaded successfully.');
-      res.json({msg: 'success'});
+      Events.create({ image: imagePath, video: videoPath, date: Date.now(), message: fields.text }, function (err, event) {
+        if (err) {
+          res.json({msg: 'error'});
+        };
+        res.json({msg: 'success'});
+      });
 
       break;
 
