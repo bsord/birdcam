70ab970b2024c8417577c9c6a4642e933a74cddc
bsord <brandon.sorgdrager@gmail.com>
2022-05-05 00:34:41 -0500

    Add cors support

--- api/app.js
+++ api/app.js
@@ -6,8 +6,14 @@ var eventsRouter = require('./routes/events');
 var devicesRouter = require('./routes/devices');
 var db = require('./storage/mongodb')
 var app = express();
+var cors = require('cors')
+
 db.connect()
 
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
